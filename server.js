ca22767415572e4eff34944234ec35484352f05f
Xueqin Li <54841759+lixueqin-4119@users.noreply.github.com>
2021-03-29 00:26:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ var server = http.createServer(function(request, response){
     <link rel="stylesheet" href="/x">
     </head>
     <body>
- <h1>标题</h1>
-<script src="/y"> </script>
+    <h1>哈哈</h1>
+    <script src="/y"> </script>
     </body>
     `)
     response.end()
